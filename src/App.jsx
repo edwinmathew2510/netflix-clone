be31677f1d0a6fbe261cb8f0662b18f2116d4a88
Edwin Mathew <edwintm2510@gmail.com>
2025-09-30 17:18:16 +0530

    Initial commit - Netflix Clone setup, NavBar added

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import NavBar from './componets/NavBar/NavBar'
+import './App.css'
+
+
+
+function App() {
+  return (
+    <div>
+      <NavBar/>
+    </div>
+  )
+}
+
+export default App
