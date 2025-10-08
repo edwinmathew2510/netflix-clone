2ecd860061eccca58a1360eb6ac78617284378e0
Edwin Mathew <edwintm2510@gmail.com>
2025-10-08 14:21:14 +0530

    Use environment variable for TMDB API key

--- src/constants/Constant.jsx
+++ src/constants/Constant.jsx
@@ -1,5 +1,5 @@
 export const baseUrl = "https://api.themoviedb.org/3";
 
-export const API_KEY='874c232a95b9bbf6c890697bb54a1f54'
+export const API_KEY = process.env.REACT_APP_API_KEY;
 
 export const imageUrl = 'https://image.tmdb.org/t/p/original'
